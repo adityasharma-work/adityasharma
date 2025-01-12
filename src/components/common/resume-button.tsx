aef2edb5fd98b4d06cd11d56e3a3bf8d6fade4bf
adityasharma-work <adityasharma01work@gmail.com>
2025-01-12 16:41:44 +0530

    resume_link_change

--- src/components/common/resume-button.tsx
+++ src/components/common/resume-button.tsx
@@ -5,7 +5,7 @@ import { Download } from "lucide-react";
 import Link from "next/link";
 
 const resumeDriveLink =
-  "https://drive.google.com/file/d/1DgJxyoNKvvhQjg9lMCETtFKO-5Qwetx3/view?usp=sharing";
+  "https://drive.google.com/file/d/17IFXPJht7FSFV4UXUUA9o4yN5HA4-sW5/view?usp=drive_link";
 
 const ResumeButton = () => {
   return (
